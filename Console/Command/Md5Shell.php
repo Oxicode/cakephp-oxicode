2fe4ac3262733f72993e256df3217c5570b23d4f
Oxicode <Oxicode@users.noreply.github.com>
2014-11-24 11:50:54 -0500

    Update Md5Shell.php

--- Console/Command/Md5Shell.php
+++ Console/Command/Md5Shell.php
@@ -24,6 +24,7 @@ App::uses('HttpSocket', 'Network/Http');
 class Md5Shell extends AppShell {
 
 	private $_url = 'http://www.md5online.org/';
+	private $ignoreMissing = false;
 
 	public function crypt() {
 		while (empty($password)) {
@@ -41,7 +42,7 @@ class Md5Shell extends AppShell {
 		$HttpSocket = new HttpSocket();
 
 		$md5 = trim($md5);
-		if(strlen($md5) !== 32) {
+		if (strlen($md5) !== 32) {
 			return $this->out('Hash invalido');
 		}
 		$matches = array();
@@ -49,33 +50,34 @@ class Md5Shell extends AppShell {
 		$html = $HttpSocket->get($this->_url);
 		$pattern = '/<input type="hidden" name="a" value="(.*)">/Uis';
 		preg_match_all($pattern, $html->body, $matches, PREG_SET_ORDER);
-		$a = $matches[0][1];
 
-		$rest = $this->postDataViaCurl($md5,$a);
+		if (empty($matches[0][1])) {
+			return $this->out('El servidor no responde');
+		}
+
+		$a = $matches[0][1];
+		$rest = $this->postDataViaCurl($HttpSocket, $md5, $a);
+		do {
+		    $this->out('..');
+			sleep(1);
+		}
+		while ($rest->code === null);
+
 		$returnArray = array();
 		$pattern2 = '/Found (.*) <b>(.*)<\/b><\/span>/Uis';
 		preg_match_all($pattern2, $rest, $returnArray, PREG_SET_ORDER);
 
-		$nt = strip_tags(@$returnArray[0][2]);
-		$nt = trim($nt);
-		if(empty($nt)) {
+		if (empty($returnArray[0][2])) {
+			return $this->out('El servidor no responde');
+		}
+		$nt = trim(strip_tags($returnArray[0][2]));
+		if (empty($nt)) {
 			return false;
 		}
 		$this->out('Contraseña desencriptada: <info>' . $nt . '</info>');
 	}
 
-/**
- *Function to post variables to a remote file using cURL
- *
- *@author Rochak Chauhan
- *@Colaborador Oxicode
- *@param string $url
- *
- *@return string
- */
-	private function postDataViaCurl($md5, $a){
-		$HttpSocket = new HttpSocket();
-
+	private function postDataViaCurl($socket, $md5, $a) {
 		$data = [
 			'md5' => $md5,
 			'search' => '0',
@@ -83,8 +85,10 @@ class Md5Shell extends AppShell {
 			'Decrypt',
 			'a' => $a
 		];
-		$results = $HttpSocket->post($this->_url, $data);
+		$results = $socket->post($this->_url, $data, ['timeout' => 10]);
 
-		return $results;
+
+		return $results->body;
 	}
+
 }
