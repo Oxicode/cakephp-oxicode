937407cb845e82d59a8d226baf91fc8d088c3e9b
Oxicode <christian.quispeh@gmail.com>
2014-12-05 15:26:31 -0500

    login

--- Config/routes.php
+++ Config/routes.php
@@ -1,8 +1,14 @@
 <?php
+	Router::connect('/api/:version/login',
+		array('plugin' => 'Oxicode', 'controller' => 'Rest', 'action' => 'login', 'ext' => 'json'),
+		array('version' => '1')
+	);
 	Router::connect('/api/:version/:noun/*',
 		array('plugin' => 'Oxicode', 'controller' => 'Rest', 'action' => 'disparador', 'ext' => 'json'),
 		array('version' => '[1-2]')
 	);
 
 	Router::mapResources('Oxicode.Rest', array('prefix' => 'api'));
-
+	Router::resourceMap(array(
+		array('action' => 'login', 'method' => 'POST', 'id' => false),
+	));
